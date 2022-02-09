3e9659471e12f809c1e1cda4d11ff95491df1d1b
Julien Monty <julien.monty@ab-cube.com>
2022-02-09 11:22:27 +0100

    Add react-base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { NativeBaseProvider, Box, Button } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar />
-    </View>
+    <NativeBaseProvider>
+      <Box alignItems="center">
+        <Button onPress={() => console.log('hello world')}>Click Me</Button>
+      </Box>
+    </NativeBaseProvider>
   );
 }
