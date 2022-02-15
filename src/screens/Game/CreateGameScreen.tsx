9b8cf17ed8eed6f6b6cfd91a82ee9e17c28bf864
Julien Monty <julien.monty@gmail.com>
2022-02-15 20:16:28 +0100

    Design new game screen

--- src/screens/Game/CreateGameScreen.tsx
+++ src/screens/Game/CreateGameScreen.tsx
@@ -1,18 +1,67 @@
 import React from 'react';
-import { View, Button, Text } from 'native-base';
+import {
+  Center, Box, VStack, FormControl, Button, Select, Radio, Badge,
+} from 'native-base';
 
 export default function CreateGameScreen() {
+  const [level, setLevel] = React.useState('1');
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Create New Game</Text>
-      <Button
-        mt="2"
-        w="100%"
-        maxW="300"
-        colorScheme="amber"
-      >
-        PLAY
-      </Button>
-    </View>
+    <Center w="100%">
+      <Box safeArea p="2" py="8" w="90%" maxW="290">
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>Variant</FormControl.Label>
+            <Select
+              selectedValue="standard"
+              isDisabled
+            >
+              <Select.Item label="Standard" value="standard" />
+            </Select>
+          </FormControl>
+          <FormControl mt="5">
+            <FormControl.Label>Time Control</FormControl.Label>
+            <Select
+              selectedValue="correspondence"
+              isDisabled
+            >
+              <Select.Item label="Correspondence" value="correspondence" />
+            </Select>
+          </FormControl>
+          <FormControl mt="5">
+            <FormControl.Label>
+              Stockfish level
+              <Badge ml="2">Elo</Badge>
+            </FormControl.Label>
+            <Radio.Group
+              value={level}
+              name="level"
+              accessibilityLabel="Select the strength"
+              onChange={(newLevel) => setLevel(newLevel)}
+            >
+              <Radio value="1" colorScheme="amber" my={1}>
+                Level 1
+                <Badge ml="2">800</Badge>
+              </Radio>
+              <Radio value="2" colorScheme="amber" my={1}>
+                Level 2
+                <Badge ml="2">1100</Badge>
+              </Radio>
+              <Radio value="3" colorScheme="amber" my={1}>
+                Level 3
+                <Badge ml="2">1400</Badge>
+              </Radio>
+              <Radio value="4" colorScheme="amber" my={1}>
+                Level 4
+                <Badge ml="2">1700</Badge>
+              </Radio>
+            </Radio.Group>
+          </FormControl>
+          <Button mt="2" colorScheme="amber">
+            Play
+          </Button>
+        </VStack>
+      </Box>
+    </Center>
   );
 }
